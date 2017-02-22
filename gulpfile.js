b4711c1313cf9a3b589a9ea5711212d5b14564a7
gggxx <170418719@qq.com>
2017-02-22 22:23:55 +0800

    add gulp-html-import

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const fs = require('fs');
 const inline = require('gulp-inline-source');
+const HTMLImport = require('gulp-html-import');
+
 
 gulp.task('less2css' , ()=>{
 	gulp.src('./css/src/*.less')
@@ -8,17 +11,21 @@ gulp.task('less2css' , ()=>{
 	    .pipe( gulp.dest( './css' ) )
 })
 
-gulp.task('inline' ,['less2css'] , ()=>{
+gulp.task('inline' ,[ 'less2css'] , ()=>{
 
 	gulp.src('./main/*.html' )
 	    .pipe( inline() )
+      .pipe( HTMLImport('./components/') )
 	    .pipe( gulp.dest( './dist' ) )
 })
 
-gulp.task( 'watch' , ()=>{
 
+gulp.task( 'watch' , ()=>{
 	gulp.watch(['./css/src/*.less','./js/*.js'],['less2css','inline']);
 })
 
+// gulp.task('default',['import'])
+gulp.task( 'default' , [ 'less2css' , 'inline' , 'watch']);
 
-gulp.task( 'default' , ['less2css' , 'inline' , 'watch']);
+
+
